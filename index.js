d3a76dd2a715fbc3421075b813479e57976d10b8
ethanpurcell <ethanpurcell@seattleacademy.org>
2017-10-09 13:28:35 -0700

    made changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -7,8 +8,19 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+function turnCube(face,amount){
+	console.log("face",face,"amount",amount);
+}
+
 io.on('connection', function(socket){
+  socket.on('turn cube', function(msg){
+  	turnCube("U","-1");
+  	console.log("turn cube",msg)
+    io.emit('turn cube', msg);
+  });
+
   socket.on('chat message', function(msg){
+  	console.log("received message",msg)
     io.emit('chat message', msg);
   });
 });
